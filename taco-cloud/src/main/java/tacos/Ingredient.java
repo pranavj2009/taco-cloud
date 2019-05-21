cab15585276bf281f645ca75412f1342a573b57c
Pranav <Pranav@LAPTOP-I6QIRHOM>
2019-05-21 19:51:01 +0530

    Lombok used at Ingredients.java

--- taco-cloud/src/main/java/tacos/Ingredient.java
+++ taco-cloud/src/main/java/tacos/Ingredient.java
@@ -1,5 +1,10 @@
 package tacos;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Data
 public class Ingredient {
 	
 	private final String id;
@@ -8,46 +13,6 @@ public class Ingredient {
 	
 	private static enum Type{
 			WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public Ingredient(String id, String name, String type) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.type = type;
-	}
-
-	@Override
-	public int hashCode() {
-		// TODO Auto-generated method stub
-		return super.hashCode();
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		// TODO Auto-generated method stub
-		return super.equals(obj);
-	}
-
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
-	}
-	
-	
-	
+	}	
 		
 }
