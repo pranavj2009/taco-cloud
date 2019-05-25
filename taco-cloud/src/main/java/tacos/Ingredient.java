0e760c47d72302e3d7fc659798b4814e762b71af
Pranav <Pranav@LAPTOP-I6QIRHOM>
2019-05-25 19:49:14 +0530

    1. Ingredient class changed for Subway sandwich ingredients. 2. DesignTacoController added.

--- taco-cloud/src/main/java/tacos/Ingredient.java
+++ taco-cloud/src/main/java/tacos/Ingredient.java
@@ -6,13 +6,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Data
 public class Ingredient {
-	
+
 	private final String id;
 	private final String name;
-	private final String type;
+	private final Type type;
+	private final FoodType foodType;
+
+	public static enum Type {
+		BREAD, BASE, VEGGIES, CHEESE, SAUCE;
+	}
+
+	public static enum FoodType {
+		VEG, NONVEG, EGG;
+	}
 	
-	private static enum Type{
-			WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE;
-	}	
-		
+	
+
 }
