0a6bf2add7733ec07b9446a1d72bbdd72b7cbb6f
Pranav <Pranav@LAPTOP-I6QIRHOM>
2019-06-02 09:58:00 +0530

    OrderForm partially created. orderForm.html tacos.web.OrderController.java tacos.Order.java

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import tacos.Ingredient.FoodType;
@@ -45,9 +46,11 @@ public class DesignTacoController {
 		return "design";
 	}
 	
+	
 	public List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
 		return ingredients.stream()
 				.filter(x -> x.getType().equals(type))
 				.collect(Collectors.toList());
 	}
+	
 }
